f569c2b754260c6c68accb967394c4c521f3bd22
Aditya <adityathakur00199.8@gamil.com>
2025-08-30 02:55:42 +0530

    created the enviormnet varibles and the basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  
   return (
     <>
-     <div className='mx-auto mt-auto bg-sky-300 text-center p-4'>
+     <div className='mx-auto bg-neutral-700 text-center p-4 '>
       Mega project 
      </div>
     </>
